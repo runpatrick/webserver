4df481e9a8be3de1b3083664262498b00a0709fb
runpatrick <34929307+runpatrick@users.noreply.github.com>
2021-08-15 01:49:00 -0400

    Create api.py

---
+++ api.py
@@ -0,0 +1,124 @@
+import pytextnow
+import sys
+
+number = sys.argv[1]
+message = sys.argv[2]
+
+#Number 1
+client = pytextnow.Client("alvarovapeshop", cookie="s%3AuO8tmZExHKPI8BHwhgt1bK8eA-uy78Vf.q5mEkCdh%2BQLQwIueotGknLQWrjy87Gz6dUocoJxZ2e8")
+client.send_sms(number, message)
+print(" Message from num1 sent to " + number + " With the message " + message)
+#Number 2
+client1 = pytextnow.Client("alvarovapeshop1", cookie="s%3AXZeAmUuIqSYhuM4WJO9Rry1lEWsV8BJG.GFM6Zonmr82JF45adxPXtaM9QR7TzoSMJvy6I3azFzI")
+client1.send_sms(number, message)
+print(" Message from num2 sent to " + number + " With the message " + message)
+#Number 3
+client2 = pytextnow.Client("venoxforo", cookie="s%3Awxy0S2cuRIkmn0c6FRdCDqffvSj7pQ0q.IfdZyLRUxtJKYT1vmYRUBdQbMS%2BREsUiehaNIjqRVcQ")
+client2.send_sms(number, message)
+print(" Message from num3 sent to " + number + " With the message " + message)
+#Number 4
+client3 = pytextnow.Client("smsmash01", cookie="s%3AOOpxludmtBfHjMChbpD_oeK6FcRModpU.IwMPUCO7NvU1prmf8ZCkYMRab3vJ9Q4uuKU1G4Z7%2Bc8")
+client3.send_sms(number, message)
+print(" Message from num4 sent to " + number + " With the message " + message)
+#Number 5
+client4 = pytextnow.Client("runpatrick17", cookie="s%3ArSnMzJe0AnxMd8PqgJemu16D-zJwC5X3.OaqMR2RU3lABb7qH6C4Ybw4vmnsZ1cmdI9Bt0SwWzTA")
+client4.send_sms(number, message)
+print(" Message from num5 sent to " + number + " With the message " + message)
+#Number 6
+client5 = pytextnow.Client("dulration", cookie="s%3A1WCqNBCdsCddfRyrG5X4FggQ0YV9WKCh.IAaSY03LOYH3D%2BoWv7U3xMQ6f3dxth%2BUdzz5rXHBB1s")
+client5.send_sms(number, message)
+print(" Message from num6 sent to " + number + " With the message " + message)
+#Number 7
+client6 = pytextnow.Client("ilovemagicsexy", cookie="s%3AXNLbNlOx2TMypVzI9w3qwi7X2GmPdjF3.5uh6s%2FZ3U0SWgN6kFCpUZDSkrlLx152ZWgytzAHbnHk")
+client6.send_sms(number, message)
+print(" Message from num7 sent to " + number + " With the message " + message)
+#Number 8
+client7 = pytextnow.Client("idontknowmyownname10", cookie="s%3AHuZcZybsHBBss9wG_sAz9HzVAQBmu414.cB9UllNXLuLe2EJr2xpLq4jAe86CyJhT0bD7sBNVhe8")
+client7.send_sms(number, message)
+print(" Message from num8 sent to " + number + " With the message " + message)
+#Number 9
+client8 = pytextnow.Client("smsmash002", cookie="s%3AHUGCbsSlKr5QDmBSwnq7CCtP2lws6zEE.PlBQYB00VTTNGYg1X6Qug4ThFmfStGOHkq4IjMA3LXQ")
+client8.send_sms(number, message)
+print(" Message from num9 sent to " + number + " With the message " + message)
+#Number 10
+client9 = pytextnow.Client("smsmash003", cookie="s%3AKAyk97yauufmY-ZgXMKU_WhVqmAgFZUL.wxe3O%2B%2BlrOLtSMMONo6MVFVNoIS4yADI4wDV%2Fjq5kmI")
+client9.send_sms(number, message)
+print(" Message from num10 sent to " + number + " With the message " + message)
+#Number 11
+client9 = pytextnow.Client("smsmash004", cookie="s%3AHcAtsk3Dxae0D49yO5AHN8tDugTtoX56.maX4Ts%2Fshm30H7WV33HcHOPI2K46BMGJgaVr2IbI3PM")
+client9.send_sms(number, message)
+print(" Message from num11 sent to " + number + " With the message " + message)
+#Number 12
+client9 = pytextnow.Client("smsmash005", cookie="s%3Alf2Tdsanc8lnQJHBEGtAhLKCLw0x3Cg7.Pd%2BJlgxuIsVl1atXJIfa3n5k5j89vJuG3FME2AI4sng")
+client9.send_sms(number, message)
+print(" Message from num12 sent to " + number + " With the message " + message)
+#Number 13
+client9 = pytextnow.Client("smsmash006", cookie="s%3AZ7S4DQM1NXv_Z22o9XhuSdzw-rG1GhZ3.BHkw8UPhFvIj2iZnwWFuyjuCLjKojb55ym1IgQscIQw")
+client9.send_sms(number, message)
+print(" Message from num13 sent to " + number + " With the message " + message)
+#Number 14
+client9 = pytextnow.Client("smsmash007", cookie="s%3AdOG7trzqVdnhJFI_uhgu1K6r4BWQwVf4.mTQ3W4UloBWlqiDF7PL749MZH2hzj6UcE1xHj%2BVo5fs")
+client9.send_sms(number, message)
+print(" Message from num14 sent to " + number + " With the message " + message)
+#Number 15
+client9 = pytextnow.Client("smsmash008", cookie="s%3AwB8d38Ai_CT9p_8FqZBcU8t09AOl6PYr.m0D2Q2QVc%2F%2F8eNgFBlAvHwMMYoggKk1pgZb17admZok")
+client9.send_sms(number, message)
+print(" Message from num15 sent to " + number + " With the message " + message)
+#Number 16
+client9 = pytextnow.Client("smsmash009", cookie="s%3A0p0Wm6cQQNWWzEyWhaan-54MdVAdxNOv.p%2FFu494EIUEcrKtoQvfMx9RxcDaJXmykgxKemuIM6uE")
+client9.send_sms(number, message)
+print(" Message from num16 sent to " + number + " With the message " + message)
+#Number 17
+client9 = pytextnow.Client("smsmash0010", cookie="s%3AZtTSu8Fc4tBR_9CRq7Pb8_Cex-un9FKn.97OTFNTxZC7euE6SFassEB5coM9SWBiJfoJYTh%2Fjd5Y")
+client9.send_sms(number, message)
+print(" Message from num17 sent to " + number + " With the message " + message)
+#Number 18
+client9 = pytextnow.Client("smsmash0011", cookie="s%3AC7tHvZNeoZkTx24_nnFj34XCbX5HWZNK.1nT3leuIzYsd5whe507KnOvvGZRBtaaDwlxgePsQvrk")
+client9.send_sms(number, message)
+print(" Message from num18 sent to " + number + " With the message " + message)
+#Number 19
+client9 = pytextnow.Client("smsmash0012", cookie="s%3AaJybUBhi_5QLLf745cdHE2OFYD4mLfZY.fmjS5kXQm%2FP1wqUDGputjve9KMxjniKimP1VKD0D1os")
+client9.send_sms(number, message)
+print(" Message from num19 sent to " + number + " With the message " + message)
+#Number 20
+client9 = pytextnow.Client("smsmash0013", cookie="s%3Av8cXaiH16azEk2K8bZfJatDg4hrSKBBy.Cf7zGpQ6b5eUaoEc8Bz4qnSaHoCm7VgYipyRlLJnKBg")
+client9.send_sms(number, message)
+print(" Message from num20 sent to " + number + " With the message " + message)
+#Number 21
+client9 = pytextnow.Client("smsmash0014", cookie="s%3AQRiP7cdvSu8JKNvj4DFX8z4EwJ2dVDpA.10VA%2BTXFOqeUrzGylOydLysKls8gOjKi0WyHp3bTaFQ")
+client9.send_sms(number, message)
+print(" Message from num21 sent to " + number + " With the message " + message)
+#Number 22
+client9 = pytextnow.Client("smsmash0015", cookie="s%3APQw3xYIm8koPOav-fVu15ljLLGFXhkmy.IMhbn%2B2hoS%2F8pGFM5Nsxrc5roNqBT%2FpjL1mLsCIX4mI")
+client9.send_sms(number, message)
+print(" Message from num22 sent to " + number + " With the message " + message)
+#Number 23
+client9 = pytextnow.Client("smsmash0016", cookie="s%3AyO6J1NB1BceE-bDyAWmKF8YxBbSS0l6e.OZIjFsUsWBI60ueduiYtht2Pcq0Afd2Hg9bu7IOI8no")
+client9.send_sms(number, message)
+print(" Message from num23 sent to " + number + " With the message " + message)
+#Number 24
+client9 = pytextnow.Client("smsmash0017", cookie="s%3Ax8jPE34cCs_gqtlE3XrN0ahWNnqpIWYE.oqKsdfhrBx5bJKtJ%2FGTbmtUTih8azmN%2BMnyL%2FQe%2BHws")
+client9.send_sms(number, message)
+print(" Message from num24 sent to " + number + " With the message " + message)
+#Number 25
+client9 = pytextnow.Client("smsmash0018", cookie="s%3AZltT7bs-PFpjYBRJZMYRCmFh0uuq1myR.kJujo79oXvVcZ5NRZDHo5Q21c1532AIvpm2FmLwpxu4")
+client9.send_sms(number, message)
+print(" Message from num25 sent to " + number + " With the message " + message)
+#Number 26
+client9 = pytextnow.Client("smsmash0019", cookie="s%3A9fkNTzdqoQXDrKG70kRJdfhPNlavYm2c.nI%2BeARsLusXSubeXV%2B8Dtu5dwTiDNt5rOTjML2wo%2Frk")
+client9.send_sms(number, message)
+print(" Message from num26 sent to " + number + " With the message " + message)
+#Number 27
+client9 = pytextnow.Client("smsmash0020", cookie="s%3AjUg6MMxDrRZXQMoIr4sescxYP_dVVLcG.vs3ABKWQaUJOET005mLJihAqwvICtxcVCK3fEH%2BBbMM")
+client9.send_sms(number, message)
+print(" Message from num27 sent to " + number + " With the message " + message)
+
+#Number 28 Not working forgort password typed wrong] 
+#client9 = pytextnow.Client("smsmash0021", cookie="s%3Av8cXaiH16azEk2K8bZfJatDg4hrSKBBy.Cf7zGpQ6b5eUaoEc8Bz4qnSaHoCm7VgYipyRlLJnKBg")
+#client9.send_sms(number, message)
+#print(" Message from num28 sent to " + number + " With the message " + message)
+
+#Number 29
+client9 = pytextnow.Client("smsmash0022", cookie="s%3A7C7cIK3lj-1Idclk5TjB11NTJuTzJH_t.%2B6Ww3XSc0%2BqtMpASpFfV0%2BHro8CGp1%2F9bLwFJys4JTI")
+client9.send_sms(number, message)
+print(" Message from num29 sent to " + number + " With the message " + message)
